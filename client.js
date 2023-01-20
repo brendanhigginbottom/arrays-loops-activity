8729d396fe2a3f7dc880581efb0aeb7978125f43
Brendan Higginbottom <brendan.higginbottom@gmail.com>
2023-01-20 17:23:19 -0600

    commiting group work

--- client.js
+++ client.js
@@ -7,6 +7,13 @@
  *    hobbies
  */
 
+let groupHobbies = ['cycling', 'gaming', 'lifting', 'coding'];
+let listNumber = 1;
+for (let element of groupHobbies) {
+    console.log(`${listNumber}: ${element}`);
+    listNumber += 1;
+}
+console.log(`Total hobbies: ${groupHobbies.length}`);
 
 // Example output
 // 1. swimming
@@ -25,6 +32,16 @@
  * 4. Output the array and number of times teal was found
  */
 
+let colors = ['orange', 'red', 'blue', 'teal'];
+let tealCount = 0;
+for (i = 0; i < colors.length; i += 1) {
+    if (colors[i] === 'teal') {
+        tealCount +=1;
+    }
+}
+
+console.log(colors);
+console.log(`Teal was found ${tealCount} time(s)`);
 
 // Example output
 // green, red, teal, orange, teal
@@ -40,6 +57,22 @@
  *    array and even numbers in the evenNumbers array.
  * 4. Output the original array, odd number array and even number array
  */
+
+let numbers = [1, 3, 6, 8, 10];
+let oddNumbers = [];
+let evenNumbers = [];
+
+for (i = 0; i < numbers.length; i++) {
+    if (numbers[i] % 2 === 0) {
+        evenNumbers.push(numbers[i])
+    }
+    else {
+        oddNumbers.push(numbers[i])
+    }
+};
+console.log(numbers);
+console.log(evenNumbers);
+console.log(oddNumbers);
 
 
 // Example output
@@ -57,6 +90,19 @@
  * 4. Output both arrays
  */
 
+let booleanArray = [true, false, false, true, true];
+let toggled = [];
+for ( let boolean of booleanArray) {
+    if (boolean === true) {
+        toggled.push(false)
+    }
+    else {
+        toggled.push(true);
+    }
+}
+console.log(booleanArray);
+console.log(toggled);
+
 
 // Example output
 // true, false, true, true
@@ -72,6 +118,12 @@
  *    NOTE: You should not need a second array here.
  * 3. Output the array
  */
+let zeroArray = [5, 6, 0, 7, 0];
+for ( i = 0; i < zeroArray.length; i += 1 ) {
+    if ( zeroArray[zeroArray.length - 1] === 0 ) {
+        zeroArray
+    }
+}
 
 
 // Example output
